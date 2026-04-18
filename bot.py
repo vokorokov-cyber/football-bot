7880c75716ef41977524415b025495cf6421b4f0
vokorokov-cyber <vokorokov@gmail.com>
2026-04-18 16:06:34 +0300

    fix render

--- bot.py
+++ bot.py
@@ -22,7 +22,25 @@ async def main():
     dp.include_router(router)
 
     print("Бот запущен...")
+    await start_web_server()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+import asyncio
+import os
+from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="Bot is running")
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", handle)
+
+    port = int(os.environ.get("PORT", 10000))
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
